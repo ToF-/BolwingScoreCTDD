4fea7f6267a61fb4d9b9b671a3b5d4a334b8b4af
Christophe Thibaut <cthibaut@octo.com>
2020-10-28 22:21:35 +0100

    added cpputest-starter project, export CPPUTEST_HOME=cpputest, added minimal test harness

---
+++ with-cpputest/test-obj/example-src/Example.d
@@ -0,0 +1,15 @@
+test-obj/example-src/Example.o: example-src/Example.c \
+  cpputest/include/CppUTest/MemoryLeakDetectorMallocMacros.h \
+  cpputest/include/CppUTest/CppUTestConfig.h \
+  cpputest/include/CppUTest/CppUTestGeneratedConfig.h \
+  cpputest/include/CppUTest/StandardCLibrary.h example-include/Example.h
+
+cpputest/include/CppUTest/MemoryLeakDetectorMallocMacros.h:
+
+cpputest/include/CppUTest/CppUTestConfig.h:
+
+cpputest/include/CppUTest/CppUTestGeneratedConfig.h:
+
+cpputest/include/CppUTest/StandardCLibrary.h:
+
+example-include/Example.h:
